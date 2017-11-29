cbc83af41dcff40369fae4676a785c2df14e6309
Michael Ashton <mashton@cmmc001.bc.rzg.mpg.de>
2017-11-29 09:11:42 +0100

    Initial commit.

---
+++ pyvaporate/build.py
@@ -0,0 +1,146 @@
+from ase.lattice.cubic import SimpleCubic, FaceCenteredCubic, BodyCenteredCubic
+
+import sys
+import math
+import numpy as np
+from random import randint
+
+
+def build_emitter(element, basis, z_axis, x_axis="auto",
+                  y_axis="auto", emitter_radius=100, emitter_side_height=50,
+                  vacuum_radius=25):
+    """
+    Build an emitter (set of nodes, TAPSim style) based on an
+    element, basis, orientation, and dimensions.
+    """
+
+
+    if x_axis == "auto" and y_axis == "auto":
+        if 0.99 < np.dot(z_axis, (1, 0, 0)) < 1.01:
+            x_axis = tuple(np.cross(z_axis, (1, 0, 0)))
+        else:
+            x_axis = tuple(np.cross(z_axis, (0, 1, 0)))
+        y_axis = tuple(np.cross(z_axis, x_axis))
+        print(x_axis, y_axis, z_axis)
+
+    R = emitter_radius + vacuum_radius
+
+    if basis.lower() == "bcc":
+        lattice = BodyCenteredCubic(
+            size=(1, 1, 1),
+            directions=[x_axis, y_axis, z_axis],
+            symbol=element
+        )
+        pts = lattice.get_positions()
+        supercell_dimensions = (
+            int(math.ceil((2*R)/max([pt[0] for pt in pts])))+1,
+            int(math.ceil((2*R)/max([pt[1] for pt in pts])))+1,
+            int(math.ceil((emitter_side_height+R)/max([pt[2] for pt in pts])))+1
+        )
+        lattice = BodyCenteredCubic(
+            size=supercell_dimensions,
+            directions=[x_axis, y_axis, z_axis],
+            symbol=element
+        )
+    elif basis.lower() == "fcc":
+        lattice = FaceCenteredCubic(
+            size=(1, 1, 1),
+            directions=[x_axis, y_axis, z_axis],
+            symbol=element
+        )
+        pts = lattice.get_positions()
+        supercell_dimensions = (
+            int(math.ceil((2*R)/max([pt[0] for pt in pts])))+1,
+            int(math.ceil((2*R)/max([pt[1] for pt in pts])))+1,
+            int(math.ceil((emitter_side_height+R)/max([pt[2] for pt in pts])))+1
+        )
+        lattice = FaceCenteredCubic(
+            size=supercell_dimensions,
+            directions=[x_axis, y_axis, z_axis],
+            symbol=element
+        )
+    elif basis.lower() == "sc":
+        lattice = SimpleCubic(  
+            size=(1, 1, 1),
+            directions=[x_axis, y_axis, z_axis],
+            symbol=element
+        )
+        pts = lattice.get_positions()
+        supercell_dimensions = (
+            int(math.ceil((2*R)/max([pt[0] for pt in pts])))+1,
+            int(math.ceil((2*R)/max([pt[1] for pt in pts])))+1,
+            int(math.ceil((emitter_side_height+R)/max([pt[2] for pt in pts])))+1
+        )
+        lattice = SimpleCubic(
+            size=supercell_dimensions,
+            directions=[x_axis, y_axis, z_axis],
+            symbol=element
+        )
+
+    pts = lattice.get_positions()
+
+    cx = np.mean([pt[0] for pt in pts])
+    cy = np.mean([pt[1] for pt in pts])
+    min_z = min([pt[2] for pt in pts])
+
+
+    emitter_points, vacuum_points, bottom_points = [], [], []
+    for pt in pts:
+        pt = [pt[0], pt[1], pt[2]-min_z]
+        if (pt[2] < 1e-5 and (pt[0]-cx)**2+(pt[1]-cy)**2<R**2):
+            pt = [pt[0], pt[1], 0.0]
+            pt = [i*1e-10 for i in pt]
+            pt.append(2)  # bottom ID
+            bottom_points.append(pt)
+        elif (pt[2]<emitter_side_height and (pt[0]-cx)**2+(pt[1]-cy)**2\
+                <emitter_radius**2):
+            pt = [i*1e-10 for i in pt]
+            pt.append(10)  # emitter ID
+            emitter_points.append(pt)
+        elif (pt[0]-cx)**2+(pt[1]-cy)**2+(pt[2]-emitter_side_height)**2\
+                <emitter_radius**2:
+            pt = [i*1e-10 for i in pt]
+            pt.append(10)  # emitter ID
+            emitter_points.append(pt)
+        elif (pt[2]<emitter_side_height and (pt[0]-cx)**2+(pt[1]-cy)**2<R**2):
+            pt = [i*1e-10 for i in pt]
+            pt.append(0)  # vacuum ID
+            vacuum_points.append(pt)
+        elif (pt[0]-cx)**2+(pt[1]-cy)**2+(pt[2]-emitter_side_height)**2<R**2:
+            pt = [i*1e-10 for i in pt]
+            pt.append(0)  # vacuum ID
+            vacuum_points.append(pt)
+
+    conc = 0.05  # 10%
+    substitution_indices = []
+    num_atoms = len(emitter_points)
+    num_substitution_atoms = math.floor(conc*num_atoms)
+    j = 0
+    while j < num_substitution_atoms:
+        x = randint(0, num_atoms)
+        if x not in substitution_indices:
+            substitution_indices.append(x)
+            j += 1
+    for i in substitution_indices:
+        emitter_points[i][3] = 11
+        
+
+    return (emitter_points, vacuum_points, bottom_points)
+
+
+def write_emitter_file(emitter_pts, vacuum_pts, bottom_pts,
+                       filename="emitter.txt"):
+    """
+    Write a TAPSim node file based on a set of nodes from
+    build_emitter().
+    """
+
+    with open("emitter.txt", "w") as e:
+        n_nodes = len(emitter_pts)+len(vacuum_pts)+len(bottom_pts)
+        e.write("ASCII {} 0 0\n".format(n_nodes))
+        for pt in emitter_pts + vacuum_pts + bottom_pts:
+                # It's required that the coordinates be
+                # separated by a tab character (^I), not
+                # by regular spaces.
+                e.write("	".join([str(i) for i in pt]))
+                e.write("\n")

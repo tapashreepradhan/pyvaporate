3b48e3d2c418c43f3825a3ffcba9aa54099b9e76
Michael Ashton <ashtonmv@gmail.com>
2017-12-01 14:44:58 +0100

    simpler build with no vacuum or bottom.

--- pyvaporate/build.py
+++ pyvaporate/build.py
@@ -12,7 +12,7 @@ MASSES = {"W": "183.85"}
 
 def build_emitter(element, basis, z_axis, filename="emitter.txt", x_axis="auto",
                   y_axis="auto", emitter_radius=100, emitter_side_height=50,
-                  vacuum_radius=25, alloy=False):
+                  alloy=False):
     """
     Build an emitter (set of nodes, TAPSim style) based on an
     element, basis, orientation, and dimensions.
@@ -27,7 +27,7 @@ def build_emitter(element, basis, z_axis, filename="emitter.txt", x_axis="auto",
             x_axis = tuple(np.cross(z_axis, (0, 1, 0)))
         y_axis = tuple(np.cross(z_axis, x_axis))
 
-    R = emitter_radius + vacuum_radius
+    R = emitter_radius
 
     if basis.lower() == "bcc":
         lattice = BodyCenteredCubic(
@@ -88,18 +88,11 @@ def build_emitter(element, basis, z_axis, filename="emitter.txt", x_axis="auto",
     min_z = min([pt[2] for pt in pts])
 
 
-    emitter_points, vacuum_points, bottom_points = [], [], []
+    emitter_points = []
     number = 0
     for pt in pts:
         pt = [pt[0], pt[1], pt[2]-min_z]
-        if (pt[2] < 1e-5 and (pt[0]-cx)**2+(pt[1]-cy)**2<R**2):
-            pt = [pt[0], pt[1], 0.0]
-            pt = [i*1e-10 for i in pt]
-            pt.append(2)  # bottom ID
-            pt.append(number)
-            number += 1
-            bottom_points.append(pt)
-        elif (pt[2]<emitter_side_height and (pt[0]-cx)**2+(pt[1]-cy)**2\
+        if (pt[2]<emitter_side_height and (pt[0]-cx)**2+(pt[1]-cy)**2\
                 <emitter_radius**2):
             pt = [i*1e-10 for i in pt]
             pt.append(10)  # emitter ID
@@ -113,18 +106,6 @@ def build_emitter(element, basis, z_axis, filename="emitter.txt", x_axis="auto",
             pt.append(number)
             number += 1
             emitter_points.append(pt)
-        elif (pt[2]<emitter_side_height and (pt[0]-cx)**2+(pt[1]-cy)**2<R**2):
-            pt = [i*1e-10 for i in pt]
-            pt.append(0)  # vacuum ID
-            pt.append(number)
-            number += 1
-            vacuum_points.append(pt)
-        elif (pt[0]-cx)**2+(pt[1]-cy)**2+(pt[2]-emitter_side_height)**2<R**2:
-            pt = [i*1e-10 for i in pt]
-            pt.append(0)  # vacuum ID
-            pt.append(number)
-            number += 1
-            vacuum_points.append(pt)
 
     if alloy:
         conc = 0.05  # 10%
@@ -143,7 +124,7 @@ def build_emitter(element, basis, z_axis, filename="emitter.txt", x_axis="auto",
     with open(filename, "w") as e:
         n_nodes = number
         e.write("ASCII {} 1 0\n".format(n_nodes))
-        for pt in emitter_points:  # + vacuum_points + bottom_points:
+        for pt in emitter_points:
                 # It's required that the coordinates be
                 # separated by a tab character (^I), not
                 # by regular spaces.
